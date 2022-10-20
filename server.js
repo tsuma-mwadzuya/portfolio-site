7ce5c0adcb19dbdc1fe5554a2425c952a27d73a0
Sam Tsuma <tsumasamuels@gmail.com>
2022-10-20 23:29:44 +0300

    initial changes - top stories

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(initial_path, "index.html"));
 })
 
+app.get('/editor', (req, res) => {
+    res.sendFile(path.join(initial_path, "editor.html"));
+})
+
 app.listen("3000", () => {
     console.log('listening.....');
 })
